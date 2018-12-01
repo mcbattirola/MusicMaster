aad51e9046fcb186054466877a326d219f39d21f
Matheus <mcbattirola@hotmail.com>
2018-12-01 00:02:23 -0200

    Basic layout. Setting up the spotify api access

--- musicmaster/src/App.js
+++ musicmaster/src/App.js
@@ -1,25 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+    }
+  }
+
+  search() {
+    console.log('this.state: ', this.state);
+    const BASE_URL = 'https://api.spotify.com/v1/search?';
+    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    const TOKEN = 'SPOTIFY TOKEN HERE';
+
+    fetch(FETCH_URL, {
+      method: 'GET',
+      headers: { 'Authorization': 'Bearer ' + TOKEN }
+    })
+      .then(response => response.json())
+      .then(json => console.log('response json: ', json))
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="App-title"> Music Master</div>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Search an artist..."
+              query={this.state.query}
+              onChange={event => { this.setState({ query: event.target.value }) }}
+              onKeyPress={event => {
+                if (event.key == 'Enter') {
+                  this.search()
+                }
+              }}
+            />
+            <InputGroup.Addon onClick={() => this.search()}>
+              <Glyphicon glyph="search"></Glyphicon>
+            </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
+
+        <div className="Profile">
+          <div>Artist Picture</div>
+          <div>Artist Name</div>
+        </div>
+
+        <div className="Gallery">
+
+        </div>
+
       </div>
     );
   }
