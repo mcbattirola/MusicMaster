0aace20f6aa6592ede0f0f6ab55a8ad339cc558f
Matheus <mcbattirola@hotmail.com>
2018-12-01 01:04:31 -0200

    Accessing spotify's API and showing data on screen. CSS changes

--- musicmaster/src/App.js
+++ musicmaster/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Profile from './Profile';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 
@@ -9,21 +10,23 @@ class App extends Component {
     super(props);
     this.state = {
       query: '',
+      artist: null
     }
   }
 
   search() {
-    console.log('this.state: ', this.state);
     const BASE_URL = 'https://api.spotify.com/v1/search?';
     const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
     const TOKEN = 'SPOTIFY TOKEN HERE';
-
     fetch(FETCH_URL, {
       method: 'GET',
       headers: { 'Authorization': 'Bearer ' + TOKEN }
     })
       .then(response => response.json())
-      .then(json => console.log('response json: ', json))
+      .then(json => {
+        const artist = json.artists.items[0];
+        this.setState({ artist }); // same name variable and state key (artist)
+      })
   }
 
 
@@ -50,11 +53,9 @@ class App extends Component {
           </InputGroup>
         </FormGroup>
 
-        <div className="Profile">
-          <div>Artist Picture</div>
-          <div>Artist Name</div>
-        </div>
-
+        <Profile
+          artist={this.state.artist}
+        />
         <div className="Gallery">
 
         </div>
